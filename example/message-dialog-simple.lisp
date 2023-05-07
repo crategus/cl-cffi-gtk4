31dd633e35d3abca68382fef0392c4ae52821830
Crategus <kaiser@crategus.com>
2023-05-07 22:28:42 +0200

    Rework the implementation of the GtkMessageDialog examples

--- example/message-dialog-simple.lisp
+++ example/message-dialog-simple.lisp
@@ -1,37 +1,35 @@
-;;;; Simple Message Dialog - 2022-11-11
+;;;; Simple Message Dialog - 2023-5-7
 
 (in-package :gtk4-example)
 
-(defun do-message-dialog-simple (&optional application)
+(defun create-message-dialog-simple (parent)
   (let ((dialog (make-instance 'gtk:message-dialog
-                               :application application
+                               :transient-for parent
                                :modal t
                                :message-type :info
-                               :transient-for (make-instance 'gtk:window)
                                :buttons :ok
                                :text "Message Dialog"
                                :secondary-text "With secondary text")))
     ;; Handler for the "response" signal of the dialog
     (g:signal-connect dialog "response"
                       (lambda (dialog response)
-                        (format t "Response is ~a" response)
+                        (format t "Response is ~a~%" response)
                         (gtk:window-destroy dialog)))
     (gtk:widget-show dialog)))
 
 ;; A variant which uses the GTK:MESSAGE-DIALOG-NEW function
-(defun do-message-dialog-simple2 (&optional application)
-  (let ((dialog (gtk:message-dialog-new (make-instance 'gtk:window)
+(defun create-message-dialog-simple2 (parent)
+  (let ((dialog (gtk:message-dialog-new parent
                                         '(:modal)
                                         :info
-                                        :ok
-                                        "Message Dialog"
-                                        nil)))
-    ;; Set more properties
-    (setf (gtk:window-application dialog) application)
+                                        :ok-cancel
+                                        "Message Dialog for ~a"
+                                        parent)))
+    ;; Set secondary text
     (setf (gtk:message-dialog-secondary-text dialog) "With secondary text")
     ;; Handler for the "response" signal of the dialog
     (g:signal-connect dialog "response"
                       (lambda (dialog response)
-                        (format t "Response is ~a" response)
+                        (format t "Response is ~a~%" response)
                         (gtk:window-destroy dialog)))
     (gtk:widget-show dialog)))
