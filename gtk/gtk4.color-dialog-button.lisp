405d1a4ea5aaae9858d21ebb56d92baecc3e543a
Crategus <kaiser@crategus.com>
2023-07-30 23:09:54 +0200

    Implementation of GtkColorDialog and GtkColorDialogButton

---
+++ gtk/gtk4.color-dialog-button.lisp
@@ -0,0 +1,206 @@
+;;; ----------------------------------------------------------------------------
+;;; gtk4.color-dialog-button.lisp
+;;;
+;;; The documentation of this file is taken from the GTK 4 Reference Manual
+;;; Version 4.10 and modified to document the Lisp binding to the GTK library.
+;;; See <http://www.gtk.org>. The API documentation of the Lisp binding is
+;;; available from <http://www.crategus.com/books/cl-cffi-gtk4/>.
+;;;
+;;; Copyright (C) 2023 Dieter Kaiser
+;;;
+;;; Permission is hereby granted, free of charge, to any person obtaining a
+;;; copy of this software and associated documentation files (the "Software"),
+;;; to deal in the Software without restriction, including without limitation
+;;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;;; and/or sell copies of the Software, and to permit persons to whom the
+;;; Software is furnished to do so, subject to the following conditions:
+;;;
+;;; The above copyright notice and this permission notice shall be included in
+;;; all copies or substantial portions of the Software.
+;;;
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+;;; THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;; DEALINGS IN THE SOFTWARE.
+;;; ----------------------------------------------------------------------------
+;;;
+;;; Types and Values
+;;;
+;;;     GtkColorDialogButton
+;;;
+;;; Accessors
+;;;
+;;;     gtk_color_dialog_button_get_dialog
+;;;     gtk_color_dialog_button_set_dialog
+;;;     gtk_color_dialog_button_get_rgba
+;;;     gtk_color_dialog_button_set_rgba
+;;;
+;;; Functions
+;;;
+;;;     gtk_color_dialog_button_new
+;;;
+;;; Properties
+;;;
+;;;     dialog
+;;;     rgba
+;;;
+;;; Hierachy
+;;;
+;;;     GObject
+;;;     ╰── GInitiallyUnowned
+;;;         ╰── GtkWidget
+;;;             ╰── GtkColorDialogButton
+;;;
+;;; Implements
+;;;
+;;;     GtkAccessible
+;;;     GtkBuildable
+;;;     GtkConstraintTarget
+;;; ----------------------------------------------------------------------------
+
+(in-package :gtk)
+
+;;; ----------------------------------------------------------------------------
+;;; GtkColorDialogButton
+;;; ----------------------------------------------------------------------------
+
+(gobject:define-g-object-class "GtkColorDialogButton" color-dialog-button
+  (:superclass widget
+   :export t
+   :interfaces ("GtkAccessible"
+                "GtkBuildable"
+                "GtkConstraintTarget")
+   :type-initializer "gtk_color_dialog_button_get_type")
+  ((dialog
+    color-dialog-button-dialog
+    "dialog" "GtkColorDialog" t t)
+   (rgba
+    color-dialog-button-rgba
+    "rgba" "GdkRGBA" t t)))
+
+#+liber-documentation
+(setf (documentation 'color-dialog-button 'type)
+ "@version{2023-7-28}
+  @begin{short}
+    The @sym{gtk:color-dialog-button} widget is a wrapped around a 
+    @class{gtk:color-dialog} object and allows to open a color chooser dialog 
+    to change the color.
+  @end{short}
+
+  @image[color-button]{Figure: GtkColorDialogButton}
+
+  It is a suitable widget for selecting a color in a preference dialog.
+  @begin[CSS nodes]{dictionary}
+    @begin{pre}
+colorbutton
+╰── button.color
+    ╰── [content]
+    @end{pre}
+    The @sym{gtk:color-dialog-button} implementation has a single CSS node with 
+    name @code{colorbutton} which contains a button node. To differentiate it 
+    from a plain @class{gtk:button} widget, it gets the @code{.color} style 
+    class.
+  @end{dictionary}
+  Since 4.10
+  @see-class{gtk:color-dialog}")
+
+;;; ----------------------------------------------------------------------------
+;;; Property and Accessor Details
+;;; ----------------------------------------------------------------------------
+
+;;; --- color-dialog-button-dialog ---------------------------------------------
+
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "dialog" 'color-dialog-button) t)
+ "The @code{dialog} property of type @class{gtk:color-dialog} (Read / Write) 
+  @br{}
+  The @class{gtk:color-dialog} object that contains parameters for the color 
+  chooser dialog.")
+
+#+liber-documentation
+(setf (liber:alias-for-function 'color-dialog-button-dialog)
+      "Accessor"
+      (documentation 'color-dialog-button-dialog 'function)
+ "@version{2023-7-28}
+  @syntax[]{(gtk:color-dialog-button-dialog object) => dialog}
+  @syntax[]{(setf (gtk:color-dialog-button-dialog object) dialog)}
+  @argument[object]{a @class{gtk:color-dialog-button} object}
+  @argument[dialog]{a @class{gtk:color-dialog} object}
+  @begin{short}
+    Accessor of the @slot[color-dialog-button]{dialog} slot of the
+    @class{gtk:color-dialog-button} class.
+  @end{short}
+  The @sym{gtk:color-dialog-button-dialog} function returns the 
+  @class{gtk:color-dialog} object. The 
+  @sym{(setf gtk:color-dialog-button-dialog)} function sets a 
+  @class{gtk:color-dialog} object to use for creating the color chooser dialog 
+  that is presented when the user clicks the button.
+
+  Since 4.10
+  @see-class{gtk:color-dialog-button}
+  @see-class{gtk:color-dialog}")
+
+;;; --- color-dialog-button-rgba -----------------------------------------------
+
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "rgba" 'color-dialog-button) t)
+ "The @code{rgba} property of type @class{gdk:rgba} (Read / Write) @br{}
+  The selected color. This property can be set to give the button its initial
+  color, and it will be updated to reflect the users choice in the color chooser
+  dialog. Listen to the \"notify::rgba\" signal to get informed about changes to
+  the buttons color. @br{}
+  Default value: @code{#s(gdk:rgba :red 0.0 :green 0.0 :blue 0.0 :alpha 0.0)}")
+
+#+liber-documentation
+(setf (liber:alias-for-function 'color-dialog-button-rgba)
+      "Accessor"
+      (documentation 'color-dialog-button-rgba 'function)
+ "@version{2023-7-28}
+  @syntax[]{(gtk:color-dialog-button-rgba object) => rgba}
+  @syntax[]{(setf (gtk:color-dialog-button-rgba object) rgba)}
+  @argument[object]{a @class{gtk:color-dialog-button} object}
+  @argument[rgba]{a @class{gdk:rgba} instance}
+  @begin{short}
+    Accessor of the @slot[color-dialog-button]{rgba} slot of the
+    @class{gtk:color-dialog-button} class.
+  @end{short}
+  The @sym{gtk:color-dialog-button-rgba} function returns the color of the
+  button. The @sym{(setf gtk:color-dialog-button-rgba)} function sets the color.
+
+  This function is what should be used to obtain the color that was chosen by
+  the user. To get informed about changes, listen to \"notify::color\" signal.
+
+  Since 4.10
+  @see-class{gtk:color-dialog-button}
+  @see-class{gdk:rgba}")
+
+;;; ----------------------------------------------------------------------------
+;;; gtk_color_dialog_button_new
+;;; ----------------------------------------------------------------------------
+
+(declaim (inline color-dialog-button-new))
+
+(defun color-dialog-button-new (&optional (dialog nil))
+ #+liber-documentation
+ "@version{2023-7-28}
+  @argument[dialog]{an optional @class{gtk:color-dialog} object to use}
+  @return{A new @class{gtk:color-dialog-button} widget.}
+  @begin{short}    
+    Creates a new @class{gtk:color-dialog-button} widget with the given 
+    @arg{dialog}.
+  @end{short}
+  You can pass @code{nil} to this function and set a @class{gtk:color-dialog}
+  object later. The button will be insensitive until that happens.  
+  @see-class{gtk:color-dialog-button}
+  @see-class{gtk:color-dialog}"
+  (if dialog
+      (make-instance 'color-dialog-button
+                     :dialog dialog)
+      (make-instance 'color-dialog-button)))
+
+(export 'color-dialog-button-new)
+
+;;; --- End of file gtk4.color-dialog-button.lisp ------------------------------
---
+++ gtk/gtk4.color-dialog.lisp
@@ -0,0 +1,290 @@
+;;; ----------------------------------------------------------------------------
+;;; gtk4.color-dialog.lisp
+;;;
+;;; The documentation of this file is taken from the GTK 4 Reference Manual
+;;; Version 4.10 and modified to document the Lisp binding to the GTK library.
+;;; See <http://www.gtk.org>. The API documentation of the Lisp binding is
+;;; available from <http://www.crategus.com/books/cl-cffi-gtk4/>.
+;;;
+;;; Copyright (C) 2023 Dieter Kaiser
+;;;
+;;; Permission is hereby granted, free of charge, to any person obtaining a
+;;; copy of this software and associated documentation files (the "Software"),
+;;; to deal in the Software without restriction, including without limitation
+;;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;;; and/or sell copies of the Software, and to permit persons to whom the
+;;; Software is furnished to do so, subject to the following conditions:
+;;;
+;;; The above copyright notice and this permission notice shall be included in
+;;; all copies or substantial portions of the Software.
+;;;
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+;;; THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;; DEALINGS IN THE SOFTWARE.
+;;; ----------------------------------------------------------------------------
+;;;
+;;; Types and Values
+;;;
+;;;     GtkColorDialog
+;;;
+;;; Accessors
+;;;
+;;;     gtk_color_dialog_get_modal
+;;;     gtk_color_dialog_set_modal
+;;;     gtk_color_dialog_get_title
+;;;     gtk_color_dialog_set_title
+;;;     gtk_color_dialog_get_with-alpha
+;;;     gtk_color_dialog_set_with-alpha
+;;;
+;;; Functions
+;;;
+;;;     gtk_color_dialog_new
+;;;     gtk_color_dialog_choose_rgba
+;;;     gtk_color_dialog_choose_rgba_finish
+;;;
+;;; Properties
+;;;
+;;;     modal
+;;;     title
+;;;     with-alpha
+;;;
+;;; Hierachy
+;;;
+;;;     GObject
+;;;     ╰── GtkColorDialog
+;;; ----------------------------------------------------------------------------
+
+(in-package :gtk)
+
+;;; ----------------------------------------------------------------------------
+;;; GtkColorDialog
+;;; ----------------------------------------------------------------------------
+
+(gobject:define-g-object-class "GtkColorDialog" color-dialog
+  (:superclass g:object
+   :export t
+   :interfaces nil
+   :type-initializer "gtk_color_dialog_get_type")
+  ((modal
+    color-dialog-modal
+    "modal" "gboolean" t t)
+   (title
+    color-dialog-title
+    "title" "gchararray" t t)
+   (with-alpha
+    color-dialog-with-alpha
+    "with-alpha" "gboolean" t t)))
+
+#+liber-documentation
+(setf (documentation 'color-dialog 'type)
+ "@version{2023-7-28}
+  @begin{short}
+    A @sym{gtk:color-dialog} object collects the arguments that are needed to
+    present a color chooser dialog to the user, such as a title for the dialog
+    and whether it should be modal.
+  @end{short}
+
+  The dialog is shown with the @fun{gtk:color-dialog-choose-rgba} function. This
+  API follows the GIO async pattern, and the result can be obtained by calling
+  the @fun{gtk:color-dialog-choose-rgba-finish} function.
+
+  See the @class{gtk:color-dialog-button} widget for a convenient control that
+  uses the @sym{gtk:color-dialog} object and presents the results.
+
+  Since 4.10
+  @see-constructor{gtk:color-dialog-new}
+  @see-slot{gtk:color-dialog-modal}
+  @see-slot{gtk:color-dialog-title}
+  @see-slot{gtk:color-dialog-with-alpha}
+  @see-class{gtk:color-dialog-button}")
+
+;;; ----------------------------------------------------------------------------
+;;; Property and Accessor Details
+;;; ----------------------------------------------------------------------------
+
+;;; --- color-dialog-modal -----------------------------------------------------
+
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "modal" 'color-dialog) t)
+ "The @code{modal} property of type @code{:boolean} (Read / Write) @br{}
+  Whether the color chooser dialog is modal. @br{}
+  Default value: @em{true}")
+
+#+ liber-documentation
+(setf (liber:alias-for-function 'color-dialog-modal)
+      "Accessor"
+      (documentation 'color-dialog-modal 'function)
+ "@version{2023-7-28}
+  @syntax[]{(gtk:color-dialog-modal object) => modal)}
+  @syntax[]{(setf (gtk:color-dialog-modal object) modal)}
+  @argument[object]{a @class{gtk:color-dialog} object}
+  @argument[modal]{a boolean whether the color chooser dialog is modal}
+  @begin{short}
+    Accessor of the @slot[gtk:color-dialog]{modal} slot of the
+    @class{gtk:color-dialog} class.
+  @end{short}
+  The @sym{gtk:color-dialog-modal} function returns whether the color chooser
+  dialog blocks interaction with the parent window while it is presented. The
+  @sym{(setf gtk:color-dialog-modal)} function sets the property.
+
+  Since 4.10
+  @see-class{gtk:color-dialog}")
+
+;;; --- color-dialog-title -----------------------------------------------------
+
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "title" 'color-dialog) t)
+ "The @code{title} property of type @code{:string} (Read / Write) @br{}
+  A title that may be shown on the color chooser dialog that is presented by
+  the @fun{gtk:color-dialog-choose-rgba} function. @br{}
+  Default value: @code{nil}")
+
+#+ liber-documentation
+(setf (liber:alias-for-function 'color-dialog-title)
+      "Accessor"
+      (documentation 'color-dialog-title 'function)
+ "@version{2023-7-28}
+  @syntax[]{(gtk:color-dialog-title object) => title)}
+  @syntax[]{(setf (gtk:color-dialog-title object) title)}
+  @argument[object]{a @class{gtk:color-dialog} object}
+  @argument[title]{a string with the title}
+  @begin{short}
+    Accessor of the @slot[gtk:color-dialog]{title} slot of the
+    @class{gtk:color-dialog} class.
+  @end{short}
+  The @sym{gtk:color-dialog-title} function returns the title that will be shown
+  on the color chooser dialog. The @sym{(setf gtk:color-dialog-title)} function
+  sets the title.
+
+  Since 4.10
+  @see-class{gtk:color-dialog}")
+
+;;; --- color-dialog-with-alpha ------------------------------------------------
+
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "with-alpha" 'color-dialog) t)
+ "The @code{with-alpha} property of type @code{:boolean} (Read / Write) @br{}
+  Whether colors may have alpha (translucency). When @code{with-alpha} is
+  @em{false}, the color that is selected will be forced to have
+  @code{alpha = 1}. @br{}
+  Default value: @em{true}")
+
+#+ liber-documentation
+(setf (liber:alias-for-function 'color-dialog-with-alpha)
+      "Accessor"
+      (documentation 'color-dialog-with-alpha 'function)
+ "@version{2023-7-28}
+  @syntax[]{(gtk:color-dialog-with-alpha object) => setting)}
+  @syntax[]{(setf (gtk:color-dialog-with-alpha object) setting)}
+  @argument[object]{a @class{gtk:color-dialog} object}
+  @argument[setting]{a boolean whether colors may have alpha}
+  @begin{short}
+    Accessor of the @slot[gtk:color-dialog]{with-alpha} slot of the
+    @class{gtk:color-dialog} class.
+  @end{short}
+  The @sym{gtk:color-dialog-with-alpha} function returns whether colors may have
+  alpha. The @sym{(setf gtk:color-dialog-with-alpha)} function sets the
+  property.
+
+  Since 4.10
+  @see-class{gtk:color-dialog}")
+
+;;; ----------------------------------------------------------------------------
+;;; gtk_color_dialog_new
+;;; ----------------------------------------------------------------------------
+
+(declaim (inline color-dialog-new))
+
+(defun color-dialog-new ()
+ #+liber-documentation
+ "@version{2023-7-28}
+  @return{A new @class{gtk:color-dialog} object.}
+  @short{Creates a new @class{gtk:color-dialog} object.}
+  @see-class{gtk:color-dialog}"
+  (make-instance 'color-dialog))
+
+(export 'color-dialog-new)
+
+;;; ----------------------------------------------------------------------------
+;;; gtk_color_dialog_choose_rgba
+;;; ----------------------------------------------------------------------------
+
+(cffi:defcfun ("gtk_color_dialog_choose_rgba" %color-dialog-choose-rgba) :void
+  (dialog (g:object color-dialog))
+  (parent (g:object window))
+  (color (g:boxed gdk:rgba))
+  (cancellable (g:object g:cancellable))
+  (func :pointer)
+  (data :pointer))
+
+(defun color-dialog-choose-rgba (dialog parent color cancellable func)
+ #+liber-documentation
+ "@version{#2023-7-28}
+  @argument[dialog]{a @class{gtk:color-dialog} object}
+  @argument[parent]{a parent @class{gtk:window} widget}
+  @argument[color]{a @class{gdk:rgba} instance with the color to select
+    initially}
+  @argument[cancellable]{a @class{g:cancellable} object to cancel the operation}
+  @argument[func]{a @symbol{g:async-ready-callback} callback function to call
+    when the operation is complete}
+  @begin{short}
+    This function initiates a color choice operation by presenting a color
+    chooser dialog to the user.
+  @end{short}
+  The callback function will be called when the dialog is dismissed. It should
+  call the @fun{gtk:color-dialog-choose-rgba-finish} function to obtain the
+  result.
+
+  Since 4.10
+  @see-class{gtk:color-dialog}
+  @see-class{gtk:window}
+  @see-class{gdk:rgba}
+  @see-class{g:cancellable}
+  @see-symbol{g:async-ready-callback}
+  @see-function{gtk:color-dialog-choose-rgba-finish}"
+  (let ((ptr (glib:allocate-stable-pointer func)))
+    (%color-dialog-choose-rgba dialog
+                               parent
+                               color
+                               cancellable
+                               (cffi:callback g:async-ready-callback)
+                               ptr)))
+
+(export 'color-dialog-choose-rgba)
+
+;;; ----------------------------------------------------------------------------
+;;; gtk_color_dialog_choose_rgba_finish
+;;; ----------------------------------------------------------------------------
+
+(cffi:defcfun ("gtk_color_dialog_choose_rgba_finish"
+               %color-dialog-choose-rgba-finish) (g:boxed gdk:rgba)
+  (dialog (g:object color-dialog))
+  (result (g:object g:async-result))
+  (err :pointer))
+
+(defun color-dialog-choose-rgba-finish (dialog result)
+ #+liber-documentation
+ "@version{#2023-7-28}
+  @argument[dialog]{a @class{gtk:color-dialog} object}
+  @argument[result]{a @class{g:async-result} instance}
+  @return{A @class{gdk:rgba} instance with the selected color, or @code{nil}.}
+  @begin{short}
+    Finishes the @fun{gtk:color-dialog-choose-rgba} function call and returns
+    the resulting color.
+  @end{short}
+
+  Since 4.10
+  @see-class{gtk:color-dialog}
+  @see-class{g:async-result}
+  @see-class{gdk:rgba}
+  @see-function{gtk:color-dialog-choose-rgba}"
+  (glib:with-ignore-g-error (err)
+    (%color-dialog-choose-rgba-finish dialog result err)))
+
+(export 'color-dialog-choose-rgba-finish)
+
+;;; --- End of file gtk4.color-dialog.lisp -------------------------------------
