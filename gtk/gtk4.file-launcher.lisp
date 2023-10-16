506b7b6db7789d5bd13794c0728f989c4e1dfd99
Crategus <kaiser@crategus.com>
2023-10-16 22:56:21 +0200

    Adding the documentation for GtkFileLauncher

--- gtk/gtk4.file-launcher.lisp
+++ gtk/gtk4.file-launcher.lisp
@@ -77,6 +77,27 @@
     file-launcher-file
     "file" "GFile" t t)))
 
+#+liber-documentation
+(setf (documentation 'file-launcher 'type)
+ "@version{#2023-10-12}
+  @begin{short}
+    A @class{gtk:file-launcher} object collects the arguments that are needed
+    to open a file with an application.
+  @end{short}
+  Depending on system configuration, user preferences and available APIs, this
+  may or may not show an app chooser dialog or launch the default application
+  right away.
+
+  The operation is started with the @fun{gtk:file-launcher-launch} function.
+  This API follows the GIO async pattern, and the result can be obtained by
+  calling the @fun{gtk:file-launcher-launch-finish} function.
+
+  To launch uris that do not represent files, use the @class{gtk:uri-launcher}
+  class.
+
+  Since 4.10
+  @see-class{gtk:uri-launcher}")
+
 ;;; ----------------------------------------------------------------------------
 ;;; Property and Accessor Details
 ;;; ----------------------------------------------------------------------------
@@ -91,24 +112,66 @@
 ;;; ----------------------------------------------------------------------------
 
 ;;; ----------------------------------------------------------------------------
-;;; Gtk.FileLauncher:file
+;;; gtk_file_launcher_get_always_ask
 ;;;
-;;; The file to launch.
+;;; Returns whether to ask the user to choose an app for opening the file.
 ;;;
-;;; Since 4.10
+;;; Since 4.12
 ;;; ----------------------------------------------------------------------------
 
 ;;; ----------------------------------------------------------------------------
+;;; gtk_file_launcher_set_always_ask
+;;;
+;;; Sets whether to awlays ask the user to choose an app for opening the file.
+;;; If FALSE, the file might be opened with a default app or the previous
+;;; choice.
+;;;
+;;; Since 4.12
+;;; ----------------------------------------------------------------------------
+
+;;; --- file-dialog-file -------------------------------------------------------
+
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "file" 'file-launcher) t)
+ "The @code{file} property of type @class{g:file} (Read / Write) @br{}
+  The file to launch.")
+
+#+liber-documentation
+(setf (liber:alias-for-function 'file-launcher-file)
+      "Accessor"
+      (documentation 'file-launcher-file 'function)
+ "@version{#2023-10-12}
+  @syntax[]{(gtk:file-launcher-file object) => file}
+  @syntax[]{(setf (gtk:file-launcher-file object) file)}
+  @argument[object]{a @class{gtk:file-launcher} object}
+  @argument[file]{a @class{g:file} object}
+  @begin{short}
+    Accessor of the @slot[gtk:file-launcher]{file} slot of the
+    @class{gtk:file-launcher} class.
+  @end{short}
+  The @fun{gtk:file-launcher-file} function gets the file that will be opened.
+  The @setf{gtk:file-launcher-file} function sets the file.
+
+  Since 4.10
+  @see-class{gtk:file-launcher}
+  @see-class{g:file}")
+
+;;; ----------------------------------------------------------------------------
 ;;; gtk_file_launcher_new
-;;;
-;;; Creates a new GtkFileLauncher object.
-;;;
-;;; Since 4.10
 ;;; ----------------------------------------------------------------------------
 
 (declaim (inline file-launcher-new))
 
 (defun file-launcher-new (&optional file)
+ #+liber-documentation
+ "@version{#2023-10-12}
+  @argument[file]{a @class{g:file} object}
+  @return{The new @class{gtk:file-launcher} object}
+  @short{Creates a new @class{gtk:file-launcher} object.}
+
+  Since 4.10
+  @see-class{gtk:file-launcher}
+  @see-class{g:file}"
   (make-instance 'file-launcher
                  :file file))
 
@@ -116,10 +179,6 @@
 
 ;;; ----------------------------------------------------------------------------
 ;;; gtk_file_launcher_launch
-;;;
-;;; Launch an application to open the file.
-;;;
-;;; Since 4.10
 ;;; ----------------------------------------------------------------------------
 
 (cffi:defcfun ("gtk_file_launcher_launch" %file-launcher-launch) :void
@@ -130,6 +189,27 @@
   (data :pointer))
 
 (defun file-launcher-launch (launcher parent cancellable func)
+ #+liber-documentation
+ "@version{#2023-10-12}
+  @argument[launcher]{a @class{gtk:file-launcher} object}
+  @argument[parent]{a @class{gtk:window} parent window}
+  @argument[cancellable]{a @class{g:cancellable} object to cancel the operation}
+  @argument[func]{a @symbol{g:async-ready-callback} callback function to call
+    when the operation is complete}
+  @begin{short}
+    Launch an application to open the file.
+  @end{short}
+  This may present an app chooser dialog to the user.
+
+  The callback will be called when the operation is completed. It should call
+  the @fun{gtk:file-launcher-launch-finish} function to obtain the result.
+
+  Since 4.10
+  @see-class{gtk:file-launcher}
+  @see-class{gtk:window}
+  @see-class{g:cancellable}
+  @see-symbol{g:async-ready-callback}
+  @see-function{gtk:file-launcher-launch-finish}"
   (let ((ptr (glib:allocate-stable-pointer func)))
     (%file-launcher-launch launcher
                            parent
@@ -141,10 +221,6 @@
 
 ;;; ----------------------------------------------------------------------------
 ;;; gtk_file_launcher_launch_finish
-;;;
-;;; Finishes the gtk_file_launcher_launch() call and returns the result.
-;;;
-;;; Since 4.10
 ;;; ----------------------------------------------------------------------------
 
 (cffi:defcfun ("gtk_file_launcher_launch-finish" %file-launcher-launch-finish)
@@ -154,6 +230,20 @@
   (err :pointer))
 
 (defun file-launcher-launch-finish (launcher result)
+ #+liber-documentation
+ "@version{#2023-10-12}
+  @argument[launcher]{a @class{gtk:file-launcher} object}
+  @argument[result]{a @class{g:async-result} object}
+  @return{@em{True} if an application was launched, or @em{false} on error.}
+  @begin{short}
+    Finishes the the @fun{gtk:file-launcher-launch} function call and returns
+    the result.
+  @end{short}
+
+  Since 4.10
+  @see-class{gtk:file-launcher}
+  @see-class{g:async-result}
+  @see-function{gtk:file-launcher-launch}"
   (glib:with-ignore-g-error (err)
     (%file-launcher-launch-finish launcher result err)))
 
@@ -161,42 +251,77 @@
 
 ;;; ----------------------------------------------------------------------------
 ;;; gtk_file_launcher_open_containing_folder
-;;;
-;;; Launch a file manager to show the file in its parent directory.
-;;;
-;;; Since 4.10
 ;;; ----------------------------------------------------------------------------
+
+(cffi:defcfun ("gtk_file_launcher_open_containing_folder"
+               %file-launcher-open-containing-folder) :void
+  (launcher (g:object file-launcher))
+  (parent (g:object window))
+  (cancellable (g:object g:cancellable))
+  (func :pointer)
+  (data :pointer))
+
+(defun file-launcher-open-containing-folder (launcher parent cancellable func)
+ #+liber-documentation
+ "@version{#2023-10-12}
+  @argument[launcher]{a @class{gtk:file-launcher} object}
+  @argument[parent]{a @class{gtk:window} parent window}
+  @argument[cancellable]{a @class{g:cancellable} object to cancel the operation}
+  @argument[func]{a @symbol{g:async-ready-callback} callback function to call
+    when the operation is complete}
+  @begin{short}
+    Launch a file manager to show the file in its parent directory.
+  @end{short}
+  This is only supported native files. It will fail if @arg{file} is e.g. a
+  @file{http://} URI.
+
+  The callback function will be called when the operation is completed. It
+  should call the @fun{gtk:file-launcher-open-containing-folder-finish} function
+  to obtain the result.
+
+  Since 4.10
+  @see-class{gtk:file-launcher}
+  @see-class{gtk:window}
+  @see-class{g:cancellable}
+  @see-symbol{g:async-ready-callback}
+  @see-function{gtk:file-launcher-open-containing-folder-finish}"
+  (let ((ptr (glib:allocate-stable-pointer func)))
+    (%file-launcher-open-containing-folder
+            launcher
+            parent
+            cancellable
+            (cffi:callback g:async-ready-callback)
+            ptr)))
+
+(export 'file-launcher-open-containing-folder)
 
 ;;; ----------------------------------------------------------------------------
 ;;; gtk_file_launcher_open_containing_folder_finish
-;;;
-;;; Finishes the gtk_file_launcher_open_containing_folder() call and returns
-;;; the result.
-;;;
-;;; Since: 4.10
 ;;; ----------------------------------------------------------------------------
 
+(cffi:defcfun ("gtk_file_launcher_open_containing_folder_finish"
+               %file-launcher-open-containing-folder-finish) :boolean
+  (launcher (g:object file-launcher))
+  (result (g:object g:async-result))
+  (err :pointer))
 
-;;;gtk_file_launcher_get_always_ask
-;;;Returns whether to ask the user to choose an app for opening the file.
+(defun file-launcher-open-containing-folder-finish (launcher result)
+ #+liber-documentation
+ "@version{#2023-10-12}
+  @argument[launcher]{a @class{gtk:file-launcher} object}
+  @argument[result]{a @class{g:async-result} object}
+  @begin{short}
+    Finishes the @fun{gtk:file-launcher-open-containing-folder} function call
+    and returns the result.
+  @end{short}
 
-;;;since: 4.12
+  Since 4.10
+  @see-class{gtk:file-launcher}
+  @see-class{g:async-result}
+  @see-function{gtk:file-launcher-open-containing-folder}"
+  (glib:with-ignore-g-error (err)
+    (%file-launcher-open-containing-folder-finish launcher result err)))
 
-;;;gtk_file_launcher_get_file
-;;;Gets the file that will be opened.
-
-;;;since: 4.10
-
-
-
-;;;gtk_file_launcher_set_always_ask
-;;;Sets whether to awlays ask the user to choose an app for opening the file. If FALSE, the file might be opened with a default app or the previous choice.
-
-;;;since: 4.12
-
-;;;gtk_file_launcher_set_file
-;;;Sets the file that will be opened.
-
-;;;since: 4.10
+(export 'file-launcher-open-containing-folder-finish)
 
 ;;; --- End of file gtk4.file-launcher.lisp ------------------------------------
