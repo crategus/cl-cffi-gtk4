02d2224a7be6c7edb6849059e6848a87c767afc3
Crategus <kaiser@crategus.com>
2023-12-17 14:46:47 +0100

    GtkListHeader: Work on the implementation and documentation

--- gtk/gtk4.list-header.lisp
+++ gtk/gtk4.list-header.lisp
@@ -31,7 +31,7 @@
 ;;;
 ;;;     GtkListHeader
 ;;;
-;;; Functions
+;;; Accessors
 ;;;
 ;;;     gtk_list_header_get_child
 ;;;     gtk_list_header_set_child
@@ -58,14 +58,6 @@
 
 ;;; ----------------------------------------------------------------------------
 ;;; GtkListHeader
-;;;
-;;; GtkListHeader is used by list widgets to represent the headers they display.
-;;;
-;;; GtkListHeader objects are managed just like GtkListItem objects via their
-;;; factory, but provide a different set of properties suitable for managing the
-;;; header instead of individual items.
-;;;
-;;; Since 4.12
 ;;; ----------------------------------------------------------------------------
 
 (gobject:define-g-object-class "GtkListHeader" list-header
@@ -89,100 +81,172 @@
     list-header-start
     "start" "guint" t nil)))
 
+#+liber-documentation
+(setf (documentation 'list-header 'type)
+ "@version{2023-11-27}
+  @begin{short}
+    The @class{gtk:list-header} object is used by list widgets to represent the
+    headers they display.
+  @end{short}
+
+  The @class{gtk:list-header} objects are managed just like
+  @class{gtk:list-item} objects via their factory, but provide a different set
+  of properties suitable for managing the header instead of individual items.
+
+  Since 4.12
+  @see-slot{gtk:list-header-child}
+  @see-slot{gtk:list-header-end}
+  @see-slot{gtk:list-header-item}
+  @see-slot{gtk:list-header-n-items}
+  @see-slot{gtk:list-header-start}
+  @see-class{gtk:list-item}")
+
 ;;; ----------------------------------------------------------------------------
 ;;; Property and Accessor Details
 ;;; ----------------------------------------------------------------------------
 
-;;; ----------------------------------------------------------------------------
-;;; Gtk.ListHeader:child
-;;;
-;;; Widget used for display.
-;;;
-;;; Since 4.12
-;;; ----------------------------------------------------------------------------
+;;; --- gtk:list-header-child --------------------------------------------------
 
-;;; ----------------------------------------------------------------------------
-;;; Gtk.ListHeader:end
-;;;
-;;; The first position no longer part of this section.
-;;;
-;;; Since 4.12
-;;; ----------------------------------------------------------------------------
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "child" 'list-header) t)
+ "The @code{child} property of type @class{gtk:widget} (Read / Write) @br{}
+  Widget used for display.")
 
-;;; ----------------------------------------------------------------------------
-;;; Gtk.ListHeader:item
-;;;
-;;; The item at the start of the section.
-;;;
-;;; Since 4.12
-;;; ----------------------------------------------------------------------------
+#+liber-documentation
+(setf (liber:alias-for-function 'list-header-child)
+      "Accessor"
+      (documentation 'list-header-child 'function)
+ "@version{2023-11-27}
+  @syntax[]{(gtk:list-header-child object) => child}
+  @syntax[]{(setf (gtk:list-header-child object) child)}
+  @argument[object]{a @class{gtk:list-header} object}
+  @argument[child]{a @class{gtk:widget} child widget}
+  @begin{short}
+    Accessor of the @slot[gtk:list-header]{child} slot of the
+    @class{gtk:list-header} class.
+  @end{short}
+  The @fun{gtk:list-header-child} function gets the child widget previously set
+  or @code{nil} if none was set. The @setf{gtk:list-header-child} function sets
+  the child widget to be used for this listitem.
 
-;;; ----------------------------------------------------------------------------
-;;; Gtk.ListHeader:n-items
-;;;
-;;; Number of items in this section.
-;;;
-;;; Since 4.12
-;;; ----------------------------------------------------------------------------
+  This function is typically called by applications when setting up a header so
+  that the widget can be reused when binding it multiple times.
 
-;;; ----------------------------------------------------------------------------
-;;; Gtk.ListHeader:start
-;;;
-;;; First position of items in this section.
-;;;
-;;; Since 4.12
-;;; ----------------------------------------------------------------------------
+  Since 4.12
+  @see-class{gtk:list-header}")
 
-;;; ----------------------------------------------------------------------------
-;;; gtk_list_header_get_child
-;;;
-;;; Gets the child previously set via gtk_list_header_set_child() or NULL if
-;;; none was set.
-;;;
-;;; Since 4.12
-;;; ----------------------------------------------------------------------------
+;;; --- gtk:list-header-end ----------------------------------------------------
 
-;;; ----------------------------------------------------------------------------
-;;; gtk_list_header_set_child
-;;;
-;;; Sets the child to be used for this listitem.
-;;;
-;;; Since 4.12
-;;; ----------------------------------------------------------------------------
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "end" 'list-header) t)
+ "The @code{end} property of type @code{guint} (Read) @br{}
+  The first position no longer part of the section. @br{}
+  Default value: @var{+gtk-invalid-list-position+}")
 
-;;; ----------------------------------------------------------------------------
-;;; gtk_list_header_get_end
-;;;
-;;; Gets the end position in the model of the section that self is currently the
-;;; header for.
-;;;
-;;; Since 4.12
-;;; ----------------------------------------------------------------------------
+#+liber-documentation
+(setf (liber:alias-for-function 'list-header-end)
+      "Accessor"
+      (documentation 'list-header-end 'function)
+ "@version{2023-11-27}
+  @syntax[]{(gtk:list-header-end object) => end}
+  @argument[object]{a @class{gtk:list-header} object}
+  @argument[end]{an unsigned integer with the end position of the section}
+  @begin{short}
+    Accessor of the @slot[gtk:list-header]{end} slot of the
+    @class{gtk:list-header} class.
+  @end{short}
+  The @fun{gtk:list-header-end} function gets the end position in the model of
+  the section that @arg{object} is currently the header for. If @arg{object} is
+  unbound, the @var{+gtk-invalid-list-position+} value is returned.
 
-;;; ----------------------------------------------------------------------------
-;;; gtk_list_header_get_item
-;;;
-;;; Gets the model item at the start of the section. This is the item that
-;;; occupies the list model at position GtkListHeader:start.
-;;;
-;;; Since 4.12
-;;; ----------------------------------------------------------------------------
+  Since 4.12
+  @see-class{gtk:list-header}
+  @see-variable{+gtk-invalid-list-position+}")
 
-;;; ----------------------------------------------------------------------------
-;;; gtk_list_header_get_n_items
-;;;
-;;; Gets the the number of items in the section.
-;;;
-;;; Since 4.12
-;;; ----------------------------------------------------------------------------
+;;; --- gtk:list-header-item ---------------------------------------------------
 
-;;; ----------------------------------------------------------------------------
-;;; gtk_list_header_get_start
-;;;
-;;; Gets the start position in the model of the section that self is currently
-;;; the header for.
-;;;
-;;; Since 4.12
-;;; ----------------------------------------------------------------------------
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "item" 'list-header) t)
+ "The @code{item} property of type @class{g:object} (Read) @br{}
+  The item at the start of the section.")
+
+#+liber-documentation
+(setf (liber:alias-for-function 'list-header-item)
+      "Accessor"
+      (documentation 'list-header-item 'function)
+ "@version{2023-11-27}
+  @syntax[]{(gtk:list-header-item object) => item}
+  @argument[object]{a @class{gtk:list-header} object}
+  @argument[item]{a @class{g:object} object with the item displayed}
+  @begin{short}
+    Accessor of the @slot[gtk:list-header]{item} slot of the
+    @class{gtk:list-header} class.
+  @end{short}
+  The @fun{gtk:list-header-item} function gets the model item at the start of
+  the section. This is the item that occupies the list model at the
+  @slot[gtk:list-header]{start} position.
+
+  If @arg{object} is unbound, this function returns @code{nil}.
+
+  Since 4.12
+  @see-class{gtk:list-header}
+  @see-function{gtk:list-header-start}")
+
+;;; --- gtk:list-header-n-items ------------------------------------------------
+
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "n-items" 'list-header) t)
+ "The @code{n-items} property of type @code{guint} (Read) @br{}
+  Number of items in the section.")
+
+#+liber-documentation
+(setf (liber:alias-for-function 'list-header-n-items)
+      "Accessor"
+      (documentation 'list-header-n-items 'function)
+ "@version{2023-11-27}
+  @syntax[]{(gtk:list-header-n-items object) => n-items}
+  @argument[object]{a @class{gtk:list-header} object}
+  @argument[n-items]{an unsigned integer with the number of items in the
+    section}
+  @begin{short}
+    Accessor of the @slot[gtk:list-header]{n-items} slot of the
+    @class{gtk:list-header} class.
+  @end{short}
+  The @fun{gtk:list-header-n-items} function gets the the number of items in
+  the section. If @arg{object} is unbound, 0 is returned.
+
+  Since 4.12
+  @see-class{gtk:list-header}
+  @see-function{gtk:list-header-start}")
+
+;;; --- gtk:list-header-start --------------------------------------------------
+
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "start" 'list-header) t)
+ "The @code{start} property of type @code{guint} (Read) @br{}
+  The first position of items in the section. @br{}
+  Default value: @var{+gtk-invalid-list-position+}")
+
+#+liber-documentation
+(setf (liber:alias-for-function 'list-header-start)
+      "Accessor"
+      (documentation 'list-header-start 'function)
+ "@version{2023-11-27}
+  @syntax[]{(gtk:list-header-start object) => start}
+  @argument[object]{a @class{gtk:list-header} object}
+  @argument[start]{an unsigned integer with the start position of the section}
+  @begin{short}
+    Accessor of the @slot[gtk:list-header]{start} slot of the
+    @class{gtk:list-header} class.
+  @end{short}
+  The @fun{gtk:list-header-start} function gets the start position in the model
+  of the section that @arg{object} is currently the header for.
+
+  If @arg{object} is unbound, the @var{+gtk-invalid-list-position+} value is
+  returned.
+
+  Since 4.12
+  @see-class{gtk:list-header}
+  @see-variable{+gtk-invalid-list-position+}")
 
 ;;; --- End of file gtk4.list-header.lisp --------------------------------------
