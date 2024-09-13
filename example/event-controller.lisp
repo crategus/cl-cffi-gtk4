f122569c92072142bba260f145a52fbc50a84a1e
Crategus <kaiser@crategus.com>
2024-09-13 19:56:51 +0200

    Start example for event controller

--- example/event-controller.lisp
+++ example/event-controller.lisp
@@ -3,52 +3,165 @@
 (in-package :gtk4-example)
 
 (defun print-controller-details (controller)
+
   (format t "  Details for the Event Controller :~%")
   (format t "        name : ~a~%"
-            (gtk-event-controller-name controller))
+            (gtk:event-controller-name controller))
   (format t "       limit : ~a~%"
-            (gtk-event-controller-propagation-limit controller))
+            (gtk:event-controller-propagation-limit controller))
   (format t "       phase : ~a~%"
-            (gtk-event-controller-propagation-phase controller))
+            (gtk:event-controller-propagation-phase controller))
   (format t "      widget : ~a~%"
-            (gtk-event-controller-widget controller))
+            (gtk:event-controller-widget controller))
   (format t "       event : ~a~%"
-            (gtk-event-controller-current-event controller))
+            (gtk:event-controller-current-event controller))
   (format t "      device : ~a~%"
-            (gtk-event-controller-current-event-device controller))
+            (gtk:event-controller-current-event-device controller))
   (format t "       state : ~a~%"
-            (gtk-event-controller-current-event-state controller))
+            (gtk:event-controller-current-event-state controller))
   (format t "        time : ~a~%"
-            (gtk-event-controller-current-event-time controller)))
+            (gtk:event-controller-current-event-time controller)))
+
+(defparameter event-controller-ui
+"<interface>
+
+  <object class='GtkBox' id='infobox'>
+
+    <property name='orientation'>vertical</property>
+
+    <child>
+      <object class='GtkBox'>
+        <property name='orientation'>horizontal</property>
+        <child>
+          <object class='GtkLabel' id='textcontroller'>
+            <property name='label'>Controller :</property>
+            <property name='halign'>GTK_ALIGN_END</property>
+          </object>
+        </child>
+        <child>
+          <object class='GtkLabel' id='labelcontroller'>
+            <property name='label'>...</property>
+            <property name='halign'>GTK_ALIGN_START</property>
+          </object>
+        </child>
+      </object>
+    </child>
+
+    <child>
+      <object class='GtkBox'>
+        <property name='orientation'>horizontal</property>
+        <child>
+          <object class='GtkLabel' id='textname'>
+            <property name='label'>name :</property>
+            <property name='halign'>GTK_ALIGN_END</property>
+          </object>
+        </child>
+        <child>
+          <object class='GtkLabel' id='labelname'>
+            <property name='label'>...</property>
+            <property name='halign'>GTK_ALIGN_START</property>
+          </object>
+        </child>
+      </object>
+    </child>
+
+  </object>
+
+  <object class='GtkSizeGroup'>
+    <property name='mode'>GTK_SIZE_GROUP_VERTICAL</property>
+    <widgets>
+      <widget name='textcontroller'/>
+      <widget name='textname'/>
+    </widgets>
+  </object>
+
+  <object class='GtkSizeGroup'>
+    <property name='mode'>GTK_SIZE_GROUP_VERTICAL</property>
+    <widgets>
+      <widget name='labelcontroller'/>
+      <widget name='labelname'/>
+    </widgets>
+  </object>
+
+<interface>")
 
 (defun do-event-controller (&optional application)
-  (let* ((vbox (make-instance 'gtk-box
+  (let* ((builder (gtk:builder-new-from-string event-controller-ui))
+         (infobox (gtk:builder-object builder "infobox"))
+
+         (area (make-instance 'gtk:drawing-area
+                               :content-width 300
+                               :content-height 300))
+         (frame1 (make-instance 'gtk:frame
+                                :child area))
+
+         (entry (make-instance 'gtk:text))
+         (frame2 (make-instance 'gtk:frame
+                                :child entry))
+
+         (vbox (make-instance 'gtk:box
+                              :orientation :vertical))
+         (hbox (make-instance 'gtk:box
                               :orientation :horizontal))
-         (window (make-instance 'gtk-window
+
+         (window (make-instance 'gtk:window
                                 :application application
-                                :child vbox
-                                :title "Event Controller")))
+                                :child hbox
+                                :title "Event Controller"))
 
-    (let* ((entry (make-instance 'gtk-text))
-           (frame (make-instance 'gtk-frame
-                                 :child entry))
-           (controller (make-instance 'gtk-event-controller-key
-                                      :name "GtkEventControllerKey")))
+         (keycontroller (make-instance 'gtk:event-controller-key
+                                       :name "GtkEventControllerKey"))
+         (motioncontroller (make-instance 'gtk:event-controller-motion
+                                          :name "GtkEventControllerMotion")))
 
-      (gtk-box-append vbox frame)
+    (gtk:box-append vbox (make-instance 'gtk:label
+                                        :label "Drawing area"))
+    (gtk:box-append vbox frame1)
+    (gtk:box-append vbox (make-instance 'gtk:label
+                                        :label "Text entry"))
+    (gtk:box-append vbox frame2)
 
-      ;; Add the key event controller to the text widget
-      (gtk-widget-add-controller entry controller)
-      ;; Handle the signals for the key event controller
-      (g-signal-connect controller "key-pressed"
-          (lambda (controller keyval keycode state)
-            (format t "~%in Signal KEY-PRESSED~%")
-            (format t "  controller : ~a~%" controller)
-            (format t "      keyval : ~a~%" keyval)
-            (format t "     keycode : ~a~%" keycode)
-            (format t "       state : ~a~%" state)
+    (gtk:box-append hbox vbox)
+    (gtk:box-append hbox infobox)
 
-            (print-controller-details controller)
+    ;; Add the motion controller to the drawing area
+    (gtk:widget-add-controller area motioncontroller)
+
+    (g:signal-connect motioncontroller "enter"
+        (lambda (controller x y)
+          (setf (gtk:label-label (gtk:builder-object builder "labelcontroller"))
+                (format nil "~a" controller))
+          (setf (gtk:label-label (gtk:builder-object builder "labelname"))
+                (format nil "~a" (gtk:event-controller-name controller)))
+
+          (format t "~%in Signal ENTER~%")
+          (format t "  controller : ~a~%" controller)
+          (format t "       (x,y) : (~a, ~a)~%" x y)))
+
+    (g:signal-connect motioncontroller "leave"
+        (lambda (controller)
+          (format t "~%in Signal LEAVE~%")
+          (format t "  controller : ~a~%" controller)))
+
+    (g:signal-connect motioncontroller "motion"
+        (lambda (controller x y)
+          (format t "~%in Signal MOTION~%")
+          (format t "  controller : ~a~%" controller)
+          (format t "       (x,y) : (~a, ~a)~%" x y)))
+
+    ;; Add the key event controller to the text widget
+    (gtk:widget-add-controller entry keycontroller)
+
+    ;; Handle the signals for the key event controller
+    (g:signal-connect keycontroller "key-pressed"
+        (lambda (controller keyval keycode state)
+          (format t "~%in Signal KEY-PRESSED~%")
+          (format t "  controller : ~a~%" controller)
+          (format t "      keyval : ~a~%" keyval)
+          (format t "     keycode : ~a~%" keycode)
+          (format t "       state : ~a~%" state)
+
+          (print-controller-details controller)
 
 ;        (format t "Details for the Event : ~%")
 ;        (let ((event (gtk-event-controller-current-event controller)))
@@ -59,16 +172,18 @@
 ;          (format t "       match : ~a~%"
 ;                    (multiple-value-list (gdk-key-event-match event))))
 
-      ))
-      (g-signal-connect controller "key-released"
-          (lambda (controller keyval keycode state)
-            (format t "~%in Signal KEY-RELEASED~%")
-            (format t "  controller : ~a~%" controller)
-            (format t "      keyval : ~a~%" keyval)
-            (format t "     keycode : ~a~%" keycode)
-            (format t "       state : ~a~%" state)
-            (print-controller-details controller)
-      )))
+    ))
+    (g:signal-connect keycontroller "key-released"
+        (lambda (controller keyval keycode state)
+          (format t "~%in Signal KEY-RELEASED~%")
+          (format t "  controller : ~a~%" controller)
+          (format t "      keyval : ~a~%" keyval)
+          (format t "     keycode : ~a~%" keycode)
+          (format t "       state : ~a~%" state)
+          (print-controller-details controller)
+    ))
 
-    ;; Show the window.
-    (gtk-widget-show window)))
+
+
+  ;; Show the window
+  (gtk:window-present window)))
