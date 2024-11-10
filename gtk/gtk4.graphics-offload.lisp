dfc33eaf5c103162b311650d183458c508111ecd
Crategus <kaiser@crategus.com>
2024-11-10 01:09:07 +0100

    Implementation of GtkGraphicsOffload

---
+++ gtk/gtk4.graphics-offload.lisp
@@ -0,0 +1,289 @@
+;;; ----------------------------------------------------------------------------
+;;; gtk4.graphics-offload.lisp
+;;;
+;;; The documentation of this file is taken from the GTK 4 Reference Manual
+;;; Version 4.16 and modified to document the Lisp binding to the GTK library.
+;;; See <http://www.gtk.org>. The API documentation of the Lisp binding is
+;;; available from <http://www.crategus.com/books/cl-cffi-gtk4/>.
+;;;
+;;; Copyright (C) 2024 Dieter Kaiser
+;;;
+;;; Permission is hereby granted, free of charge, to any person obtaining a
+;;; copy of this software and associated documentation files (the "Software"),
+;;; to deal in the Software without restriction, including without limitation
+;;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;;; and/or sell copies of the Software, and to permit persons to whom the
+;;; Software is furnished to do so, subject to the following conditions:
+;;;
+;;; The above copyright notice and this permission notice shall be included in
+;;; all copies or substantial portions of the Software.
+;;;
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+;;; THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;; DEALINGS IN THE SOFTWARE.
+;;; ----------------------------------------------------------------------------
+;;;
+;;; Types and values
+;;;
+;;;     GtkGraphicsOffload
+;;;     GtkGraphicsOffloadEnabled
+;;;
+;;; Accessors
+;;;
+;;;     gtk_graphics_offload_get_black_background
+;;;     gtk_graphics_offload_set_black_background
+;;;     gtk_graphics_offload_get_child
+;;;     gtk_graphics_offload_set_child
+;;;     gtk_graphics_offload_get_enabled
+;;;     gtk_graphics_offload_set_enabled
+;;;
+;;; Functions
+;;;
+;;;     gtk_graphics_offload_new
+;;;
+;;; Properties
+;;;
+;;;     black-background
+;;;     child
+;;;     enabled
+;;;
+;;; Object Hierarchy
+;;;
+;;;     GObject
+;;;     ╰── GInitiallyUnowned
+;;;         ╰── GtkWidget
+;;;             ╰── GtkGraphicsOffload
+;;;
+;;; Implemented Interfaces
+;;;
+;;;     GtkAccessible
+;;;     GtkBuildable
+;;;     GtkConstraintTarget
+;;; ----------------------------------------------------------------------------
+
+(in-package :gtk)
+
+;;; ----------------------------------------------------------------------------
+;;; GtkGraphicsOffloadEnabled
+;;; ----------------------------------------------------------------------------
+
+(gobject:define-genum "GtkGraphicsOffloadEnabled" graphics-offload-enabled
+  (:export t
+   :type-initializer "gtk_graphics_offload_enabled_get_type")
+  (:enabled 0)
+  (:disabled 1))
+
+#+liber-documentation
+(setf (liber:alias-for-symbol 'graphics-offload-enabled)
+      "GEnum"
+      (liber:symbol-documentation 'graphics-offload-enabled)
+ "@version{2024-11-9}
+  @begin{declaration}
+(gobject:define-genum \"GtkGraphicsOffloadEnabled\" graphics-offload-enabled
+  (:export t
+   :type-initializer \"gtk_graphics_offload_enabled_get_type\")
+  (:enabled 0)
+  (:disabled 1))
+  @end{declaration}
+  @begin{values}
+    @begin[code]{table}
+      @entry[:enabled]{Graphics offloading is enabled.}
+      @entry[:disabled]{Graphics offloading is disabled.}
+    @end{table}
+  @end{values}
+  @begin{short}
+    Represents the state of graphics offloading.
+  @end{short}
+
+  Since 4.14
+  @see-class{gtk:video}")
+
+;;; ----------------------------------------------------------------------------
+;;; GtkGraphicsOffload
+;;; ----------------------------------------------------------------------------
+
+(gobject:define-gobject "GtkGraphicsOffload" graphics-offload
+  (:superclass widget
+   :export t
+   :interfaces ("GtkAccessible" "GtkBuildable" "GtkConstraintTarget")
+   :type-initializer "gtk_graphics_offload_get_type")
+  ((black-background
+    graphics-offload-black-background
+    "black-background" "gboolean" t t)
+   (child
+    graphics-offload-child
+    "child" "GktWidget" t t)
+   (enabled
+    graphics-offload-enabled
+    "enabled" "GtkGraphicsOffloadEnabled" t t)))
+
+#+liber-documentation
+(setf (documentation 'graphics-offload 'type)
+ "@version{2024-11-9}
+  @begin{short}
+    The @class{gtk:graphics-offload} widget is a widget that allows to bypass
+    GSK rendering for its child by passing the content directly to the
+    compositor.
+  @end{short}
+  Graphics offload is an optimization to reduce overhead and battery use that
+  is most useful for video content. It only works on some platforms and in
+  certain situations. GTK will automatically fall back to normal rendering if
+  it does not.
+
+  Graphics offload is most efficient if there are no controls drawn on top of
+  the video content. You should consider using graphics offload for your main
+  widget if it shows frequently changing content, such as a video, or a VM
+  display, and you provide the content in the form of dma-buf textures, see the
+  @class{gdk:dmabuf-texture-builder} documentation, in particular if it may be
+  fullscreen.
+
+  Numerous factors can prohibit graphics offload:
+  @begin{itemize}
+    @item{Unsupported platforms. Currently, graphics offload only works on Linux
+      with Wayland.}
+    @item{Clipping, such as rounded corners that cause the video content to not
+      be rectangular.}
+    @item{Unsupported dma-buf formats, see the @fun{gdk:display-dmabuf-formats}
+      function.}
+    @item{Translucent video content, content with an alpha channel, even if it
+    is not used.}
+    @item{Transforms that are more complex than translations and scales.}
+    @item{Filters such as opacity, grayscale or similar.}
+  @end{itemize}
+  The GTK inspector provides a visual debugging tool for graphics offload.
+
+  Since 4.14
+  @see-constructor{gtk:graphics-offload-new}
+  @see-slot{gtk:graphics-offload-black-background}
+  @see-slot{gtk:graphics-offload-child}
+  @see-slot{gtk:graphics-offload-enabled}
+  @see-class{gtk:video}")
+
+;;; ----------------------------------------------------------------------------
+;;; Property and Accessor Details
+;;; ----------------------------------------------------------------------------
+
+;;; --- gtk:graphics-offload-black-background ----------------------------------
+
+#+(and gtk-4-16 liber-documentation)
+(setf (documentation (liber:slot-documentation "black-background"
+                                               'graphics-offload) t)
+ "The @code{black-background} property of type @code{:boolean} (Read / Write)
+  @br{}
+  Whether to draw a black background. Since 4.16 @br{}
+  Default value: @em{false}")
+
+#+(and gtk-4-16 liber-documentation)
+(setf (liber:alias-for-function 'graphics-offload-black-background)
+      "Accessor"
+      (documentation 'graphics-offload-black-background 'function)
+ "@version{2024-11-9}
+  @syntax{(gtk:graphics-offload-black-background object) => setting}
+  @syntax{(setf (gtk:graphics-offload-black-background object) setting)}
+  @argument[object]{a @class{gtk:graphics-offload} widget}
+  @argument[setting]{a boolean whether to draw a black background behind
+    the content}
+  @begin{short}
+    Accessor of the @slot[gtk:graphics-offload]{black-background} slot of the
+    @class{gtk:graphics-offload} class.
+  @end{short}
+  The @fun{gtk:graphics-offload-black-background} function returns whether the
+  widget draws a black background. The
+  @setf{gtk:graphics-offload-black-background} function sets whether
+  @arg{object} will draw a black background.
+
+  A main use case for this is letterboxing where black bars are visible next to
+  the content if the aspect ratio of the content does not match the dimensions
+  of the monitor. Using this property for letterboxing instead of CSS allows
+  compositors to show content with maximum efficiency, using direct scanout to
+  avoid extra copies in the compositor.
+
+  On Wayland, this is implemented using the single-pixel buffer protocol.
+
+  Since 4.16
+  @see-class{gtk:graphics-offload}")
+
+;;; --- gtk:graphics-offload-child ---------------------------------------------
+
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "child" 'graphics-offload) t)
+ "The @code{child} property of type @class{gtk:widget} (Read / Write) @br{}
+  The child widget.")
+
+#+liber-documentation
+(setf (liber:alias-for-function 'graphics-offload-child)
+      "Accessor"
+      (documentation 'graphics-offload-child 'function)
+ "@version{2024-11-9}
+  @syntax{(gtk:graphics-offload-child object) => child}
+  @syntax{(setf (gtk:graphics-offload-child object) child)}
+  @argument[object]{a @class{gtk:graphics-offload} widget}
+  @argument[child]{a @class{gtk:widget} child widget}
+  @begin{short}
+    Accessor of the @slot[gtk:graphics-offload]{child} slot of the
+    @class{gtk:graphics-offload} class.
+  @end{short}
+  The @fun{gtk:graphics-offload-child} function gets the child widget. The
+  @setf{gtk:graphics-offload-child} function sets the child widget.
+
+  Since 4.14
+  @see-class{gtk:graphics-offload}
+  @see-class{gtk:widget}")
+
+;;; --- gtk:graphics-offload-enabled -------------------------------------------
+
+#+liber-documentation
+(setf (documentation (liber:slot-documentation "enabled" 'graphics-offload) t)
+ "The @code{enabled} property of type @symbol{gtk:graphics-offload-enabled}
+  (Read / Write) @br{}
+  Whether graphics offload is enabled. @br{}
+  Default value: @code{:enabled}")
+
+#+liber-documentation
+(setf (liber:alias-for-function 'graphics-offload-enabled)
+      "Accessor"
+      (documentation 'graphics-offload-enabled 'function)
+ "@version{2024-11-9}
+  @syntax{(gtk:graphics-offload-enabled object) => setting}
+  @syntax{(setf (gtk:graphics-offload-enabled object) setting)}
+  @argument[object]{a @class{gtk:graphics-offload} widget}
+  @argument[setting]{a @symbol{gtk:graphics-offload-enabled} value}
+  @begin{short}
+    Accessor of the @slot[gtk:graphics-offload]{enabled} slot of the
+    @class{gtk:graphics-offload} class.
+  @end{short}
+  The @fun{gtk:graphics-offload-enabled} function returns whether offload is
+  enabled. The @setf{gtk:graphics-offload-enabled} function sets whether the
+  @class{gtk:graphics-offload} widget will attempt to offload the content of
+  its child widget.
+
+  Since 4.14
+  @see-class{gtk:graphics-offload}
+  @see-symbol{gtk:graphics-offload-enabled}")
+
+;;; ----------------------------------------------------------------------------
+;;; gtk_graphics_offload_new
+;;; ----------------------------------------------------------------------------
+
+(defun graphics-offload-new (&optional child)
+ #+liber-documentation
+ "@version{2024-11-9}
+  @argument[child]{an optional @class{gtk:widget} child widget}
+  @return{The new @class{gtk:graphics-offload} widget.}
+  @begin{short}
+    Creates a new @class{gtk:graphics-offload} widget.
+  @end{short}
+
+  Since 4.14
+  @see-class{gtk:graphics-offload}
+  @see-class{gtk:widget}"
+  (make-instance 'graphics-offload
+                 :child child))
+
+(export 'graphics-offload-new)
+
+;;; --- End of file gtk4.graphics-offload.lisp ---------------------------------
---
+++ test/rtest-gtk4-graphics-offload.lisp
@@ -0,0 +1,116 @@
+(in-package :gtk-test)
+
+(def-suite gtk-graphics-offload :in gtk-suite)
+(in-suite gtk-graphics-offload)
+
+;;; --- Types and values -------------------------------------------------------
+
+;;;     GtkGraphicsOffloadEnabled
+
+(test gtk-graphics-offload-enabled
+  ;; Check type
+  (is (g:type-is-enum "GtkGraphicsOffloadEnabled"))
+  ;; Check type initializer
+  (is (eq (g:gtype "GtkGraphicsOffloadEnabled")
+          (g:gtype (cffi:foreign-funcall "gtk_graphics_offload_enabled_get_type"
+                                         :size))))
+  ;; Check registered name
+  (is (eq 'gtk:graphics-offload-enabled
+          (glib:symbol-for-gtype "GtkGraphicsOffloadEnabled")))
+  ;; Check names
+  (is (equal '("GTK_GRAPHICS_OFFLOAD_ENABLED" "GTK_GRAPHICS_OFFLOAD_DISABLED")
+             (glib-test:list-enum-item-names "GtkGraphicsOffloadEnabled")))
+  ;; Check values
+  (is (equal '(0 1)
+             (glib-test:list-enum-item-values "GtkGraphicsOffloadEnabled")))
+  ;; Check nick names
+  (is (equal '("enabled" "disabled")
+             (glib-test:list-enum-item-nicks "GtkGraphicsOffloadEnabled")))
+  ;; Check enum definition
+  (is (equal '(GOBJECT:DEFINE-GENUM "GtkGraphicsOffloadEnabled"
+                                    GTK:GRAPHICS-OFFLOAD-ENABLED
+                       (:EXPORT T
+                        :TYPE-INITIALIZER
+                        "gtk_graphics_offload_enabled_get_type")
+                       (:ENABLED 0)
+                       (:DISABLED 1))
+             (gobject:get-gtype-definition "GtkGraphicsOffloadEnabled"))))
+
+;;;     GtkGraphicsOffload
+
+(test gtk-graphics-offload-class
+  ;; Check type
+  (is (g:type-is-object "GtkGraphicsOffload"))
+  ;; Check registered name
+  (is (eq 'gtk:graphics-offload
+          (glib:symbol-for-gtype "GtkGraphicsOffload")))
+  ;; Check type initializer
+  (is (eq (g:gtype "GtkGraphicsOffload")
+          (g:gtype (cffi:foreign-funcall "gtk_graphics_offload_get_type" :size))))
+  ;; Check parent
+  (is (eq (g:gtype "GtkWidget")
+          (g:type-parent "GtkGraphicsOffload")))
+  ;; Check children
+  (is (equal '()
+             (glib-test:list-children "GtkGraphicsOffload")))
+  ;; Check interfaces
+  (is (equal '("GtkAccessible" "GtkBuildable" "GtkConstraintTarget")
+             (glib-test:list-interfaces "GtkGraphicsOffload")))
+  ;; Check properties
+  (is (equal '("black-background" "child" "enabled")
+             (glib-test:list-properties "GtkGraphicsOffload")))
+  ;; Check signals
+  (is (equal '()
+             (glib-test:list-signals "GtkGraphicsOffload")))
+  ;; Check CSS name
+  (is (string= "graphicsoffload"
+               (gtk:widget-class-css-name "GtkGraphicsOffload")))
+  ;; Check accessible role
+  (is (eq :widget (gtk:widget-class-accessible-role "GtkGraphicsOffload")))
+  ;; Check class definition
+  (is (equal '(GOBJECT:DEFINE-GOBJECT "GtkGraphicsOffload" GTK:GRAPHICS-OFFLOAD
+                       (:SUPERCLASS GTK:WIDGET
+                        :EXPORT T
+                        :INTERFACES
+                        ("GtkAccessible" "GtkBuildable" "GtkConstraintTarget")
+                        :TYPE-INITIALIZER "gtk_graphics_offload_get_type")
+                       ((BLACK-BACKGROUND GRAPHICS-OFFLOAD-BLACK-BACKGROUND
+                         "black-background" "gboolean" T T)
+                        (CHILD GRAPHICS-OFFLOAD-CHILD "child" "GtkWidget" T T)
+                        (ENABLED GRAPHICS-OFFLOAD-ENABLED "enabled"
+                         "GtkGraphicsOffloadEnabled" T T)))
+             (gobject:get-gtype-definition "GtkGraphicsOffload"))))
+
+;;; --- Properties -------------------------------------------------------------
+
+;;;     black-background
+;;;     child
+;;;     enabled
+
+(test gtk-graphics-offload-properties
+  (let ((offload (make-instance 'gtk:graphics-offload)))
+    (is-false (gtk:graphics-offload-black-background offload))
+    (is-false (gtk:graphics-offload-child offload))
+    (is (eq :enabled (gtk:graphics-offload-enabled offload)))
+    (is (= 1 (g:object-ref-count offload)))))
+
+;;; --- Functions --------------------------------------------------------------
+
+;;;     gtk_graphics_offload_new
+
+(test gtk-graphics-offload-new.1
+  (let (offload)
+    (is (typep (setf offload (gtk:graphics-offload-new)) 'gtk:graphics-offload))
+    (is (= 1 (g:object-ref-count offload)))))
+
+(test gtk-graphics-offload-new.2
+  (let ((video(gtk:video-new))
+        offload)
+    (is (typep (setf offload
+                     (gtk:graphics-offload-new video)) 'gtk:graphics-offload))
+    ;; Check memory management
+    (is-false (setf (gtk:graphics-offload-child offload) nil))
+    (is (= 1 (g:object-ref-count video)))
+    (is (= 1 (g:object-ref-count offload)))))
+
+;;; 2024-11-2
