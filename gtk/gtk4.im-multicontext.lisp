85647f90cf8e1c19922ad57bf17bc1ae71e648fb
Crategus <kaiser@crategus.com>
2026-02-03 17:06:33 +0100

    Add implementation of gtk:im-multicontext-context-id

--- gtk/gtk4.im-multicontext.lisp
+++ gtk/gtk4.im-multicontext.lisp
@@ -2,11 +2,11 @@
 ;;; gtk4.im-multicontext.lisp
 ;;;
 ;;; The documentation in this file is taken from the GTK 4 Reference Manual
-;;; version 4.18 and modified to document the Lisp binding to the GTK library,
+;;; version 4.20 and modified to document the Lisp binding to the GTK library,
 ;;; see <http://www.gtk.org>. The API documentation for the Lisp binding is
 ;;; available at <http://www.crategus.com/books/cl-cffi-gtk4/>.
 ;;;
-;;; Copyright (C) 2011 - 2025 Dieter Kaiser
+;;; Copyright (C) 2011 - 2026 Dieter Kaiser
 ;;;
 ;;; Permission is hereby granted, free of charge, to any person obtaining a
 ;;; copy of this software and associated documentation files (the "Software"),
@@ -63,7 +63,7 @@
 
 #+liber-documentation
 (setf (documentation 'im-multicontext 'type)
- "@version{2023-08-29}
+ "@version{2026-01-31}
   @begin{short}
     The @class{gtk:im-multicontext} class is an input method context supporting
     multiple, switchable input methods.
@@ -81,7 +81,7 @@
 
 (defun im-multicontext-new ()
  #+liber-documentation
- "@version{2023-08-29}
+ "@version{2026-01-31}
   @return{The new @class{gtk:im-multicontext} object.}
   @begin{short}
     Creates a new input method context supporting multiple, switchable
@@ -93,37 +93,33 @@
 (export 'im-multicontext-new)
 
 ;;; ----------------------------------------------------------------------------
-;;; gtk_im_multicontext_get_context_id ()
-;;;
-;;; const char *
-;;; gtk_im_multicontext_get_context_id (GtkIMMulticontext *context);
-;;;
-;;; Gets the id of the currently active delegate of the context .
-;;;
-;;; context :
-;;;     a GtkIMMulticontext
-;;;
-;;; Returns :
-;;;     the id of the currently active delegate
+;;; gtk_im_multicontext_get_context_id
+;;; gtk_im_multicontext_set_context_id
 ;;; ----------------------------------------------------------------------------
 
-;;; ----------------------------------------------------------------------------
-;;; gtk_im_multicontext_set_context_id ()
-;;;
-;;; void
-;;; gtk_im_multicontext_set_context_id (GtkIMMulticontext *context,
-;;;                                     const char *context_id);
-;;;
-;;; Sets the context id for context .
-;;;
-;;; This causes the currently active delegate of context to be replaced by the
-;;; delegate corresponding to the new context id.
-;;;
-;;; context :
-;;;     a GtkIMMulticontext
-;;;
-;;; context_id :
-;;;     the id to use
-;;; ----------------------------------------------------------------------------
+(defun (setf im-multicontext-context-id) (id context)
+  (cffi:foreign-funcall "gtk_im_multicontext_set_context_id"
+                        (g:object im-multicontext) context
+                        :string id
+                        :void)
+  id)
+
+(cffi:defcfun ("gtk_im_multicontext_get_context_id"
+               im-multicontext-context-id) :string
+ #+liber-documentation
+ "@version{2026-01-31}
+  @syntax{(gtk:im-multicontext-context-id context) => id}
+  @syntax{(setf (gtk:im-multicontext-context-id content) id)}
+  @argument[context]{a @class{gtk:im-multicontext} object}
+  @argument[id]{a string for the context ID}
+  @begin{short}
+    Gets or sets the ID of the currently active delegate of the context.
+  @end{short}
+  Setting the context causes the currently active delegate of @arg{context} to
+  be replaced by the delegate corresponding to the new context ID.
+  @see-class{gtk:im-multicontext}"
+  (context (g:object im-multicontext)))
+
+(export 'im-multicontext-context-id)
 
 ;;; --- End of file gtk4.im-multicontext.lisp ----------------------------------
